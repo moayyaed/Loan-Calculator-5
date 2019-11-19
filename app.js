cb809c83f14749b0bf266f378ae65404891894f2
Ali Apple <toqeer.94@gmail.com>
2019-11-20 04:56:55 +0500

    Completed

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+
+    // hide results initially
+    document.getElementById('results').style.display = 'none';
+
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+    
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+
+});
 
 
-function calculateResults(e){
+function calculateResults(){
 
-    console.log('Calculating...');
     // UI Vars
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -24,16 +36,31 @@ function calculateResults(e){
       monthlyPayment.value = monthly.toFixed(2);
       totalPayment.value = (monthly * calculatedPayments).toFixed(2);
       totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+
+        // show resulsts
+        document.getElementById('results').style.display = 'block';
+    
+        // hide loader
+        document.getElementById('loading').style.display = 'none';
+
+
     } else {
       showError('Please check your numbers');
+      
+       
     }
-  
-    e.preventDefault();
+
 }
 
 
 // Show Error
 function showError(error){
+
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'none';
+    
+
     // Create a div
     const errorDiv = document.createElement('div');
   
